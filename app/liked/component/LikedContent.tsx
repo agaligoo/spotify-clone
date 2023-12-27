3511e9b3562a89c6d21a0c53b2fd1121e9f1fbbe
agaligoo <nuttasit.yg1@gmail.com>
2023-12-27 20:36:59 +0700

    fix error likecontent

--- app/liked/component/LikedContent.tsx
+++ app/liked/component/LikedContent.tsx
@@ -1,60 +1,62 @@
 "use client";
 
-
-import LikeButton from "@/components/LikeButton";
-import MediaItem from "@/components/MediaItem";
-import useOnplay from "@/hooks/useOnplay";
-import { useUser } from "@/hooks/useUser";
-import { Song } from "@/types";
-
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import {  useEffect } from "react";
 
-interface LikeContentProps {
-    songs:Song[];
-}
+import { Song } from "@/types";
+import { useUser } from "@/hooks/useUser";
+import MediaItem from "@/components/MediaItem";
+import LikeButton from "@/components/LikeButton";
+import useOnPlay from "@/hooks/useOnplay";
 
-const LikedContent: React.FC<LikeContentProps> = ({
-    songs
+interface LikedContentProps {
+  songs: Song[];
+};
+
+const LikedContent: React.FC<LikedContentProps> = ({
+  songs
 }) => {
-    const router = useRouter
-    const {isLoading,user} = useUser();
+  const router = useRouter();
+  const { isLoading, user } = useUser();
 
-    const onPlay = useOnplay(songs);
+  const onPlay = useOnPlay(songs);
 
-    useEffect(()=>{
-        if (!isLoading && !user) {
-            router.replace('/');
-        }
-    },[isLoading,user,router]);
-
-    if (songs.length === 0){
-        return (
-            <div className="
-             flex
-             flex-col
-             gap-y-2
-             w-full
-             px-6
-             text-neutral-400
-            ">
-                No liked Songs
-            </div>
-        )
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace('/');
     }
+  }, [isLoading, user, router]);
 
-    return ( 
-        <div className="flex flex-col gap-y-2 w-full p-6">
-            {songs.map((song:any)=>(
-                <div key={song.id} className="flex items-center gap-x-4 w-full">
-                    <div className="flex-1">
-                        <MediaItem data={song} onClick={(id:string)=>onPlay(id)}/>
-                    </div>
-                    <LikeButton songId={song.id}/>
-                </div>
-            ))}
+  if (songs.length === 0) {
+    return (
+      <div 
+        className="
+          flex 
+          flex-col 
+          gap-y-2 
+          w-full px-6 
+          text-neutral-400
+        "
+      >
+        No liked songs.
+      </div>
+    )
+  }
+  return ( 
+    <div className="flex flex-col gap-y-2 w-full p-6">
+      {songs.map((song: any) => (
+        <div 
+          key={song.id} 
+          className="flex items-center gap-x-4 w-full"
+        >
+          <div className="flex-1">
+            <MediaItem onClick={(id) => onPlay(id)} data={song} />
+          </div>
+          <LikeButton songId={song.id} />
         </div>
-     );
+      ))}
+    </div>
+  );
 }
  
 export default LikedContent;
